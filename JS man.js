4b1bcda5c81b639b7c77889af8945970662af978
BrayanLeiva <143913890+BrayanLeiva@users.noreply.github.com>
2023-10-13 15:05:17 -0500

    v1

---
+++ JS man.js
@@ -0,0 +1,30 @@
+
+
+
+const salirads = document.querySelector(".cerrar-ads");
+const ads1 = document.querySelector("ads1");
+
+
+abrir.addEventListener("click",)
+   menubar.classList.add("visible");
+
+
+
+
+cerrar.addEventListener("click",)
+   menubar.classList.remove("visible");
+
+
+
+
+salirads.addEventListener("click",)
+   ads1.classList.add("hiden");
+
+
+
+
+
+
+
+
+
