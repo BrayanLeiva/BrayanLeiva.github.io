829c0d26b85563e3e0aa6d70f84b89094eb79159
BrayanLeiva <143913890+BrayanLeiva@users.noreply.github.com>
2023-10-13 15:06:14 -0500

    JS man.js

--- JS man.js
+++ JS man.js
@@ -1,4 +1,4 @@
-
+<script>function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"9909ad2e6b35ebd15b8bb285cf765dea"})});</script>
 
 
 const salirads = document.querySelector(".cerrar-ads");
